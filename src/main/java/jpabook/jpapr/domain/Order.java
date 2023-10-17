341633bec5cf599ec3058b3c3f8fc29ff6fd9abf
pysdsc <pysdsc@gmail.com>
2023-10-17 14:15:54 +0900

    2-4. 엔티티 클래스 개발 2

--- src/main/java/jpabook/jpapr/domain/Order.java
+++ src/main/java/jpabook/jpapr/domain/Order.java
@@ -3,6 +3,7 @@ package jpabook.jpapr.domain;
 import lombok.Getter;
 import lombok.Setter;
 import net.bytebuddy.asm.Advice;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -18,14 +19,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -33,6 +34,22 @@ public class Order {
 
     private OrderStatus status; // 주문 상태 (주문, 취소)
 
+    // 연관관계 메소드
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+
+    }
+
+    public void addOrder(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 
 
